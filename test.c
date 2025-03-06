c0c0f42ffef11af6a0ef35d0ffb4001da7fae34a
haruki <71203445+HarukiK0324@users.noreply.github.com>
2025-03-06 14:41:56 +0900

    modified

--- test.c
+++ test.c
@@ -18,6 +18,7 @@ int	main(void)
 	t_vars vars;
 
 	vars.mlx = mlx_intit();
+	vars.mlx_win = mlx_new_window(vars.mlx, 960, 540, "Hello world!");
 	mlx_loop_hook(vars.mlx, render_next_frame, &vars);
 	mlx_loop(vars.mlx);
 }
